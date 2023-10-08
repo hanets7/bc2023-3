6a8080811478b1a05a1c55a9b0ee1b9c51aee8fb
Mykola Hanets <144606328+hanets7@users.noreply.github.com>
2023-10-08 13:54:20 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,49 +1,29 @@
 const fs = require('fs');
 
-// Функція для читання JSON-файлу
-function readJSONFile(filename) {
-  try {
-    const data = fs.readFileSync(filename, 'utf8');
-    return JSON.parse(data);
-  } catch (error) {
-    console.error(Помилка при читанні файлу ${filename}: ${error.message});
-    process.exit(1);
+fs.readFile('data.json', 'utf8', (err, data) => {
+  if (err) {
+    console.error('Помилка читання файлу:', err);
+    return;
   }
-}
-
-// Функція для запису результатів у файл
-function writeResultsToFile(results) {
   try {
-    fs.writeFileSync('output.txt', results.join('\n'), 'utf8');
-    console.log('Результати успішно записані у файл output.txt');
-  } catch (error) {
-    console.error(Помилка при записі у файл output.txt: ${error.message});
-  }
-}
+    const jsonData = JSON.parse(data);
+    const results = [];
 
-// Головна функція для аналізу JSON та запису результатів
-function analyzeAndWriteResults(data) {
-  const results = [];
-
-  // Проходимося по кожному об'єкту у JSON-файлі
-  data.forEach(item => {
-    // Перевіряємо, чи є у полі "parent" значення "BS3_BanksLiab"
-    if (item.parent === "BS3_BanksLiab") {
-      // Додаємо результат до масиву
-      results.push(${item.txt}: ${item.value});
+    for (const item of jsonData) {
+      if (item.parent === 'BS3_BanksLiab') {
+        results.push(${item.txt}:${item.value});
+      }
     }
-  });
 
-  // Записуємо результати у файл
-  writeResultsToFile(results);
-}
-
-// Головна функція програми
-function main() {
-  const filename = 'data.json';
-  const jsonData = readJSONFile(filename);
-  analyzeAndWriteResults(jsonData);
-}
-
-// Викликаємо головну функцію програми
-main();
+    fs.writeFile('output.txt', results.join('\n'), (err) => {
+      if (err) {
+        console.error('Помилка запису у файл:', err);
+        return;
+      }
+      console.log('Результати записано у файл output.txt');
+    });
+  } catch (error) {
+    console.error('Помилка обробки JSON:', error);
+  }
+});
+  
